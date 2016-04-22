87aefc43d7ed753ca6aa8ab0efe16cba88db5669
nonickels <littman58@gmail.com>
2016-04-22 00:18:19 -0400

    // Assignment 12

---
+++ src/sections/variables/StillUsingVariables.java
@@ -0,0 +1,16 @@
+package sections.variables;
+
+/**
+ * Created by Nick on 4/21/16.
+ */
+public class StillUsingVariables {
+    public static void main( String[] args ) {
+        int gradYear;
+        String name;
+
+        gradYear = 2018;
+        name = "Nicholas Littman";
+
+        System.out.println("My name is " + name + " and I will graduate in " + gradYear + ".");
+    }
+}
