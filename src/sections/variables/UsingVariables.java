1d70f92e4b8072aaddcc13de2c48d05ea274ee43
nonickels <littman58@gmail.com>
2016-04-21 23:53:21 -0400

    // Assignment 11

---
+++ src/sections/variables/UsingVariables.java
@@ -0,0 +1,21 @@
+package sections.variables;
+
+/**
+ * Created by Nick on 4/21/16.
+ */
+public class UsingVariables {
+    public static void main( String[] args ) {
+        String compSci;
+        int roomNum;
+        double eVal;
+
+        compSci = "Computer Science";
+        roomNum = 113;
+        eVal = 2.71828;
+
+        System.out.println("This is room # " + roomNum);
+        System.out.println("e is close to " + eVal);
+        System.out.println("I am learning a bit about " + compSci);
+    }
+}
+
