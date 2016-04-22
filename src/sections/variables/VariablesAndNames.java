770cec25f12542e5a99582fdf007e6f5eb87c6d5
nonickels <littman58@gmail.com>
2016-04-21 22:03:32 -0400

    // Cmd + K (Trying a commit)

--- src/sections/variables/VariablesAndNames.java
+++ src/sections/variables/VariablesAndNames.java
@@ -17,7 +17,7 @@ public class VariablesAndNames {
         cars_driven = drivers;
         carpool_capacity = cars_driven * space_in_a_car;
         average_passengers_per_car = passengers / cars_driven;
-
+        // Command + K will commit changes
 
         System.out.println( "There are " + cars + " cars available." );
         System.out.println( "There are only " + drivers + " drivers available." );
