339a19446778ddf7e600ffea1597335f8330bd1f
nonickels <littman58@gmail.com>
2016-04-22 01:16:08 -0400

    // Assignment 13

---
+++ src/sections/variables/YourSchedule.java
@@ -0,0 +1,41 @@
+package sections.variables;
+
+/**
+ * Created by Nick on 4/22/16.
+ */
+public class YourSchedule {
+    public static void main(String[] args){
+        String course1, course2, course3, course4, course5, course6, course7, course8, professor1, professor2, professor3, professor4, professor5, professor6, professor7, professor8;
+
+        course1 = "Business Communications";
+        professor1 = "Carolyn Kerr";
+        course2 = "Quantitative Methods";
+        professor2 = "Gajanan Hegde";
+        course3 = "Intro to Finance";
+        professor3 = "Andy Koch";
+        course4 = "Intro to Marketing";
+        professor4 = "Kristyn Maryott";
+        course5 = "Managerial Ethics";
+        professor5 = "Supriya Singh";
+        course6 = "Financial Accounting";
+        professor6 = "Elise Boyas";
+        course7 = "Organizational Behavior";
+        professor7 = "Ron Johnson";
+        course8 = "Managerial Economics";
+        professor8 = "Andrew R. Blair";
+
+        System.out.println("___________________________________________________");
+        System.out.println("  |                          |                    |");
+        System.out.println(" #|       Course Title" + "       |      Professor     |");
+        System.out.println("__|__________________________|____________________|");
+        System.out.println("1 |   " + course1 + "|" + "        " + professor1 + "|");
+        System.out.println("2 |      " + course2 + "|" + "       " + professor2 + "|");
+        System.out.println("3 |          " + course3 + "|" + "           " + professor3 + "|");
+        System.out.println("4 |        " + course4 + "|" + "     " + professor4 + "|");
+        System.out.println("5 |         " + course5 + "|" + "       " + professor5 + "|");
+        System.out.println("6 |      " + course6 + "|" + "         " + professor6 + "|");
+        System.out.println("7 |   " + course7 + "|" + "         " + professor7 + "|");
+        System.out.println("8 |      " + course8 + "|" + "     " + professor8 + "|");
+        System.out.println("__|__________________________|____________________|");
+    }
+}
