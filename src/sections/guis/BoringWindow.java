cd67879780396d576a853f641383726ac6a906b2
nonickels <littman58@gmail.com>
2016-04-23 19:12:29 -0400

    // Assignment 30 -> https://programmingbydoing.com/a/boring-window.html

---
+++ src/sections/guis/BoringWindow.java
@@ -0,0 +1,18 @@
+package sections.guis;
+import javax.swing.*;
+
+/**
+ * Created by Nick on 4/23/16.
+ */
+
+public class BoringWindow extends JFrame
+{
+    public static void main(String[] args)
+    {
+        JFrame f = new BoringWindow();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(300, 200);
+        f.setVisible(true);
+    }
+
+}
