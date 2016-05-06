7008e7b954f56e29f324bcbfd9e009b1ae1e3f83
Nick Littman <littman58@gmail.com>
2016-05-06 08:36:16 -0400

    // Assignment 38 -> https://programmingbydoing.com/a/compareto-challenge.html

---
+++ src/sections/ifStatements/CompareToChallenge.java
@@ -0,0 +1,62 @@
+package sections.ifStatements;
+
+/**
+ * Created by Nick on 5/2/16.
+ */
+public class CompareToChallenge
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Comparing \"friend\" with \"friendly\" produces ");
+        int a = "friend".compareTo("friendly");
+        System.out.println(a);
+
+        System.out.print("Comparing \"angry\" with \"angryfan\" produces ");
+        int b = "angry".compareTo("angryfan");
+        System.out.println(b);
+
+        System.out.print("Comparing \"pringles\" with \"pringlescan\" produces ");
+        int c = "pringles".compareTo("pringlescan");
+        System.out.println(c);
+
+        System.out.print("Comparing \"nasty\" with \"nastyapple\" produces ");
+        int d = "nasty".compareTo("nastyapple");
+        System.out.println(d);
+
+        System.out.print("Comparing \"dingle\" with \"dingleberry\" produces ");
+        int e = "dingle".compareTo("dingleberry");
+        System.out.println(e);
+
+        System.out.print("Comparing \"quartz\" with \"bread\" produces ");
+        int f = "quartz".compareTo("bread");
+        System.out.println(f);
+
+        System.out.print("Comparing \"land\" with \"goop\" produces ");
+        int g = "lang".compareTo("goop");
+        System.out.println(g);
+
+        System.out.print("Comparing \"run\" with \"kilt\" produces ");
+        int h = "run".compareTo("kilt");
+        System.out.println(h);
+
+        System.out.print("Comparing \"thinktank\" with \"think\" produces ");
+        int i = "thinktank".compareTo("think");
+        System.out.println(i);
+
+        System.out.print("Comparing \"alleycat\" with \"alley\" produces ");
+        int j = "alleycat".compareTo("alley");
+        System.out.println(j);
+
+        System.out.print("Comparing \"kanye\" with \"kanye\" produces ");
+        int k = "kanye".compareTo("kanye");
+        System.out.println(k);
+
+        System.out.println();
+        System.out.println("lol i mean");
+        System.out.println();
+
+        System.out.print("Comparing \"god\" with \"god\" produces ");
+        int l = "god".compareTo("god");
+        System.out.println(l);
+    }
+}
--- src/sections/ifStatements/GenderGame.java
+++ src/sections/ifStatements/GenderGame.java
@@ -9,7 +9,7 @@ public class GenderGame
     {
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.print("What is your gender (M or F) :");
+        System.out.print("What is your gender (m or f) :");
         String userGender = keyboard.next().toLowerCase();
 
         System.out.print("First name: ");
@@ -26,22 +26,21 @@ public class GenderGame
         if(userAge < 20)
             System.out.println("Then I shall call you " + userFirstName + " " + userLastName + ".");
 
-        if(userAge >= 20 && userGender .equals("m"))
+        if(userAge >= 20)
+        {
+            if (userAge >= 20 && userGender .equals("m"));
             System.out.println("Then I shall call you Mr. " + userLastName + ".");
 
-        if(userAge >= 20 && userGender .equals("f"))
-        {
-            System.out.println("Are you married, " + userFirstName + " (y or n)?");
-            String userMaritalStatus = keyboard.next();
-            {
-                if(userMaritalStatus .equals("y"))
-                    System.out.println("Then I shall call you Mrs. " + userLastName + ".");
-                else
-                    System.out.println("Then I shall call you Ms. " + userLastName + ".");
+            if (userAge >= 20 && userGender.equals("f")) {
+                System.out.println("Are you married, " + userFirstName + " (y or n)?");
+                String userMaritalStatus = keyboard.next();
+                {
+                    if (userMaritalStatus.equals("y"))
+                        System.out.println("Then I shall call you Mrs. " + userLastName + ".");
+                    else
+                        System.out.println("Then I shall call you Ms. " + userLastName + ".");
+                }
             }
         }
-
-
-
     }
 }
