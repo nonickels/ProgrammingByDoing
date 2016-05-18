f7132ceec8d335ce396fbf5d673dada7194183d6
Nick Littman <littman58@gmail.com>
2016-05-18 19:15:51 -0400

    // Assignment 55 -> https://programmingbydoing.com/a/adding-values-in-a-loop.html

---
+++ src/sections/doWhileLoops/DoWhileSwimming.java
@@ -0,0 +1,7 @@
+package sections.doWhileLoops;
+
+/**
+ * Created by Nick on 5/18/16.
+ */
+public class DoWhileSwimming {
+}
---
+++ src/sections/whileLoops/AddingValuesInALoop.java
@@ -0,0 +1,34 @@
+package sections.whileLoops;
+import java.util.Scanner;
+
+/**
+ * Created by Nick on 5/18/16.
+ */
+public class AddingValuesInALoop
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        int numInput;
+        int numInputTotal = 0;
+
+        System.out.println("I will add up the numbers you input.");
+        System.out.print("Number: ");
+        numInput = keyboard.nextInt();
+        numInputTotal += numInput;
+        System.out.println("The total so far is " + numInputTotal);
+
+        while (!(numInput == 0))
+        {
+            System.out.print("Number: ");
+            numInput = keyboard.nextInt();
+            numInputTotal += numInput;
+            System.out.println("The total so far is " + numInputTotal);
+        }
+
+        System.out.println();
+        System.out.println("The total is " + numInputTotal);
+
+    }
+}
