c170cef637aa9972716eebdd5ec884dbfffb92ca
Nick Littman <littman58@gmail.com>
2016-05-27 10:19:39 -0400

    // Assignment 67 ->https://programmingbydoing.com/a/ten-times.html

---
+++ src/forLoops/TenTimes.java
@@ -0,0 +1,15 @@
+package forLoops;
+
+/**
+ * Created by Nick on 5/27/16.
+ */
+public class TenTimes
+{
+    public static void main(String[] args)
+    {
+        for(int x=1; x <= 10; x = x+1)
+        {
+            System.out.println(x + ". Mr. Mitchell is cool!");
+        }
+    }
+}
