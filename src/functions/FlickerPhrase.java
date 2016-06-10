e3e50dc9d662df25e65ce07a0afe9d7c02d3bf2a
Nick Littman <littman58@gmail.com>
2016-06-10 19:35:30 -0400

    FlickerPhrase.java

---
+++ src/functions/FlickerPhrase.java
@@ -0,0 +1,62 @@
+package functions;
+
+import java.util.Random;
+/**
+ * Created by Nick on 6/10/16.
+ */
+public class FlickerPhrase
+{
+    public static void main( String[] args ) throws InterruptedException {
+        Random rng = new Random();
+        int r;
+
+        for (int i = 0; i < 100000; i++) {
+            r = 1 + rng.nextInt(5);
+            // Write five if statements here.
+            // If r is 1, then call the function named 'first'.
+            if (r == 1)
+                first();
+                // If r is 2, then call the function named 'second', and so on.
+            else if (r == 2)
+                second();
+            else if (r == 3)
+                third();
+            else if (r == 4)
+                fourth();
+            else if (r == 5)
+                fifth();
+
+
+            // Optional: after the if statements are over, add in a slight delay.
+            Thread.sleep(1500);
+
+
+            System.out.println("I pledge allegiance to the flag.");
+        }
+    }
+
+    public static void first()
+    {
+        System.out.print("I                               \r");
+    }
+
+    public static void second()
+    {
+        System.out.print("  pledge                        \r");
+    }
+
+    public static void third()
+    {
+        System.out.print("         allegiance             \r");
+    }
+
+    public static void fourth()
+    {
+        System.out.print("                    to the      \r");
+    }
+
+    public static void fifth()
+    {
+        System.out.print("                           flag.\r");
+    }
+}
