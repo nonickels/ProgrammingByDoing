56efe4599833d085483e9d43a64a716480dce5aa
Nick Littman <littman58@gmail.com>
2016-06-11 19:00:22 -0400

    keychains

--- src/functions/Keychains3.java
+++ src/functions/Keychains3.java
@@ -1,10 +1,11 @@
 package functions;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 /**
  * Created by Nick on 6/11/16.
  */
-public class Keychains2
+public class Keychains3
 {
     public static Scanner keyboard = new Scanner(System.in);
 
@@ -12,6 +13,9 @@ public class Keychains2
     public static int menuDecision = 0;
     public static int keychainCounter = 0;
     public static final int keychainPrice = 10;
+    public static final double salesTax = .0825;
+    public static final int shippingCost = 5;
+    public static final int additionalPerKeychainShippingCost = 1;
 
     public static void main(String[] args)
     {
@@ -37,13 +41,27 @@ public class Keychains2
                 case 2:
                     System.out.println("You currently have " + keychainCounter + " keychains. How many to remove?");
                     int sub = keyboard.nextInt();
-                    remove_keychains(sub);
-                    System.out.println("You now have " + keychainCounter + " keychains.");
-                    System.out.println();
+                    if (keychainCounter > 0) {
+                        remove_keychains(sub);
+                        System.out.println("You now have " + keychainCounter + " keychains.");
+                        System.out.println();
+                        break;
+                    }
+                    else if (keychainCounter <= 0)
+                    {
+                        System.out.println("You must have at least 1 keychain to remove.");
+                        System.out.println();
+                    }
                     break;
                 case 3:
                     view_order();
                     break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("ERROR.");
+                    System.out.println();
+                    continue;
             }
         }
         checkout();
@@ -65,18 +83,24 @@ public class Keychains2
     }
     public static void view_order()
     {
+        int shippingTotal = shippingCost + (keychainCounter * additionalPerKeychainShippingCost);
+        int subTotalPreTax = (keychainCounter * keychainPrice) + shippingTotal;
+        double taxAmount = subTotalPreTax * salesTax;
+        double finalCost = subTotalPreTax + (taxAmount);
+
         System.out.println("You have " + keychainCounter + " keychains.");
         System.out.println("Keychains cost $" + keychainPrice + " each.");
-        System.out.println("Total cost is $" + keychainCounter * keychainPrice + ".");
+        System.out.println("Shipping cost: $" + shippingTotal);
+        System.out.println("Subtotal before tax: $" + subTotalPreTax);
+        System.out.println("Tax: $" + taxAmount);
+        System.out.println("Final Cost: $" + finalCost);
         System.out.println();
     }
     public static void checkout()
     {
         System.out.println("What is your name?");
         String name = keyboard.next();
-        System.out.println("You have " + keychainCounter + " keychains.");
-        System.out.println("Keychains cost $" + keychainPrice + " each.");
-        System.out.println("Total cost is $" + keychainCounter * keychainPrice + ".");
+        view_order();
         System.out.println("Thanks for ordering, " + name + ".");
     }
 }
